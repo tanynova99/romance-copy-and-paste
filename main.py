56e8ac28f57211000962865ef1be38b7258a487b
tanynova69 <90447391+tanynova99@users.noreply.github.com>
2023-05-20 12:10:37 +0300

    main.py file

---
+++ main.py
@@ -0,0 +1,29 @@
+from googletrans import Translator
+import streamlit as st
+
+st.title('Да, они действительно настолько похожи! :pencil:')
+
+st.markdown("Ты должен был слышать, **насколько** похожи *романские* языки.")
+st.markdown("Напомню, что к **:blue[романским языкам]** относятся **:red[французский, итальянский, "
+            "испанский, португальский, румынский, молдавский, каталанский, провансальский,]** "
+            "сардинский, рето-романский, македоно-румынский и мертвые латинский, "
+            "оскский, умбрский и другие (так называемые италийские).")
+st.markdown('Чтобы уж точно это показать, наш дорогой робот Чаппи  покажет тебе '
+            'перевод любой фразы с русского на несколько романских языков сразу. :star:')
+
+# source language
+src = "ru"
+# target languages
+dst = ["es", "it", "fr", "pt", "ca", "la"]
+
+form = st.form(key='my_form')
+source = form.text_input(label="Напиши что-нибудь на русском:")
+# reruns the code
+submit_button = form.form_submit_button(label='Submit')
+
+translator = Translator()
+
+if submit_button:
+    for lang in dst:
+        result = translator.translate(source, dest=lang)
+        st.write(result.dest, " > ", result.text)
